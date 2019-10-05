1ed44c9bad3b03e92f1d4cb164c523d3a96b2f61
emillypedrassini <emillypedrassini@gmail.com>
2019-10-05 17:19:51 -0300

    init project

---
+++ API/sakuraerrorcaptor/src/main/java/br/com/quintoandar/sakuraerrorcaptor/SakuraerrorcaptorApplication.java
@@ -0,0 +1,13 @@
+package br.com.quintoandar.sakuraerrorcaptor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakuraerrorcaptorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakuraerrorcaptorApplication.class, args);
+	}
+
+}
