867a868b3b362296106b501b3e3f120e09dc6d9c
natalia-alarcon <nf-alarcon@uol.com.br>
2019-11-16 17:45:18 -0300

    feito logOccurrence

---
+++ API/sakuraerrorcaptor/src/main/java/br/com/quintoandar/sakuraerrorcaptor/repository/LogOccurrenceRepository.java
@@ -0,0 +1,20 @@
+package br.com.quintoandar.sakuraerrorcaptor.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.quintoandar.sakuraerrorcaptor.model.Environment;
+import br.com.quintoandar.sakuraerrorcaptor.model.Level;
+import br.com.quintoandar.sakuraerrorcaptor.model.LogOccurrence;
+
+@Repository
+public interface LogOccurrenceRepository extends JpaRepository<LogOccurrence, Long>{
+	public List<LogOccurrence>findByLogIdAndOccurrenceId(Long logId,Long occurrenceId);
+	public List<LogOccurrence>findByOccurrenceDetail(String detail);
+	public List<LogOccurrence>findByOccurrenceTitle(String title);
+	public List<LogOccurrence>findByLogLevel(Level level);
+	public List<LogOccurrence>findByLogEnvironment(Environment environment);
+	public List<LogOccurrence>findByLogEnvironmentAndLogLevel(Environment environment, Level level);
+}
---
+++ API/sakuraerrorcaptor/src/main/java/br/com/quintoandar/sakuraerrorcaptor/service/LogOccurrenceImpl.java
@@ -0,0 +1,64 @@
+package br.com.quintoandar.sakuraerrorcaptor.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.quintoandar.sakuraerrorcaptor.model.Environment;
+import br.com.quintoandar.sakuraerrorcaptor.model.Level;
+import br.com.quintoandar.sakuraerrorcaptor.model.LogOccurrence;
+import br.com.quintoandar.sakuraerrorcaptor.repository.LogOccurrenceRepository;
+import br.com.quintoandar.sakuraerrorcaptor.service.interfaces.LogOccurrenceService;
+
+@Service
+public class LogOccurrenceImpl implements LogOccurrenceService{
+
+	@Autowired
+	LogOccurrenceRepository logOccurrenceRepository;
+	
+	@Override
+	public Optional<LogOccurrence> findById(Long id) {
+		// TODO Auto-generated method stub
+		return logOccurrenceRepository.findById(id);
+	}
+
+	@Override
+	public List<LogOccurrence> findAll() {
+		// TODO Auto-generated method stub
+		return logOccurrenceRepository.findAll();
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		logOccurrenceRepository.deleteById(id);
+	}
+
+	@Override
+	public LogOccurrence save(LogOccurrence logoccurrence) {
+		// TODO Auto-generated method stub
+		return logOccurrenceRepository.save(logoccurrence);
+	}
+
+	@Override
+	public List<LogOccurrence> findByLevel(Level level) {
+		// TODO Auto-generated method stub
+		return logOccurrenceRepository.findByLogLevel(level);
+	}
+
+	@Override
+	public List<LogOccurrence> findByEnvironment(Environment environment) {
+		// TODO Auto-generated method stub
+		return logOccurrenceRepository.findByLogEnvironment(environment);
+	}
+
+	@Override
+	public List<LogOccurrence> findByEnvironmentAndLevel(Environment environment,Level level) {
+		// TODO Auto-generated method stub
+		return logOccurrenceRepository.findByLogEnvironmentAndLogLevel(environment, level);
+	}
+	
+	
+}
