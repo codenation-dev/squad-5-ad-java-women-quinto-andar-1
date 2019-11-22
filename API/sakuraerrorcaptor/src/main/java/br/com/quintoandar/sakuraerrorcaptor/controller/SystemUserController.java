34c94deffe219566c1e16a023ae2aac21ee52b65
Wendy-Anna <wendyufpb@gmail.com>
2019-11-22 08:54:13 -0300

    Correções Post

--- API/sakuraerrorcaptor/src/main/java/br/com/quintoandar/sakuraerrorcaptor/controller/SystemUserController.java
+++ API/sakuraerrorcaptor/src/main/java/br/com/quintoandar/sakuraerrorcaptor/controller/SystemUserController.java
@@ -86,12 +86,10 @@ public class SystemUserController {
 	
 
 	@PostMapping("/{id}")
-			@ApiOperation("Change password")
-			@ApiResponses(value = {@ApiResponse(code = 200, message="User alterado"), @ApiResponse(code = 404, message="User doesn't exist")})
-			public ResponseEntity<Iterable<SystemUser>> alter(@PathVariable Long id, String password) {
-				
-			//Quando coloco systemUserServices.alter(systemUser); ele dá erro...
-			systemUserServices.alter(null);
-				return new ResponseEntity<>(HttpStatus.OK);
+	@ApiOperation("Change password")
+	@ApiResponses(value = {@ApiResponse(code = 200, message="User alterado"), @ApiResponse(code = 404, message="User doesn't exist")})
+	public ResponseEntity<SystemUser> alter(@PathVariable Long id,SystemUser systemUser ,String password) {
+		systemUserServices.alter(systemUser);
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
 }
